42f9d30f5fe4c247c1ef57f13eb81cefb6f7caa0
NataNoEsta <shotaoouji@gmail.com>
2023-01-15 17:06:43 -0300

    update script

--- calculator/script.js
+++ calculator/script.js
@@ -1,20 +1,21 @@
 class Calculadora {
-    constructor(prevResultText, resultText) {
+    constructor(prevResultText, currentResultText) {
         //se inicializan los constructores:
         this.prevResultText = prevResultText
-        this.resultText = resultText
+        this.currentResultText = currentResultText
         this.clear()
     }
+    // métodos
     clear() {
-        this.resultText = ''
-        this.prevResultText = ''
+        this.result = ''
+        this.prevResult = ''
         this.operation = undefined
     }
     borrar() {
 
     }
     appendNum(number) {
-
+        this.result = number
     }
     selectOp(op) {
 
@@ -23,19 +24,28 @@ class Calculadora {
 
     }
     update() {
-
+        this.currentResultText.innerText = this.result
     }
 }
 
 
-//queryselectorAll selecciona todos los elementos del mismo tipo
+// queryselectorAll selecciona todos los elementos del mismo tipo
+// el navegador retorna un objeto nodo (nodeList object)
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalButton = document.querySelector('[data-equals]')
 const clearEveryButton = document.querySelector('[data-clear-everything]')
 const clearButton = document.querySelector('[data-clear]')
 const prevResultText = document.querySelector('[data-pre-result]')
-const resultText = document.querySelector('[data-result]')
+const currentResultText = document.querySelector('[data-result]')
 
 //se crea una instancia de la clase Calculadora
-const calculator = new Calculadora(prevResultText, resultText)
+const calculator = new Calculadora(prevResultText, currentResultText)
+
+// for each recorre la lista de nodos (cada boton) y le asigna un eventHandler
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.appendNum(button.innerText)
+        calculator.update()
+    })
+})
