e0ff8a08697b037b15bf123178c3d0ef945a7467
NataNoEsta <shotaoouji@gmail.com>
2023-01-15 10:45:37 -0300

    changed buttons order

--- calculator/script.js
+++ calculator/script.js
@@ -1,21 +1,41 @@
+class Calculadora {
+    constructor(prevResultText, resultText) {
+        //se inicializan los constructores:
+        this.prevResultText = prevResultText
+        this.resultText = resultText
+        this.clear()
+    }
+    clear() {
+        this.resultText = ''
+        this.prevResultText = ''
+        this.operation = undefined
+    }
+    borrar() {
 
-function suma(a,b) {
-    return a + b
-};
-function resta(a,b) {
-    return a - b
-};
-function division(a,b) {
-    return a/b
+    }
+    appendNum(number) {
 
-};
-function multiplicacion(a,b){
-    return a*b
-};
+    }
+    selectOp(op) {
 
-function clear() {
-    return null
-};
-function clearEverything() {
-    return null
+    }
+    calcular(){
+
+    }
+    update() {
+
+    }
 }
+
+
+//queryselectorAll selecciona todos los elementos del mismo tipo
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelector('[data-equals]')
+const clearEveryButton = document.querySelector('[data-clear-everything]')
+const clearButton = document.querySelector('[data-clear]')
+const prevResultText = document.querySelector('[data-pre-result]')
+const resultText = document.querySelector('[data-result]')
+
+//se crea una instancia de la clase Calculadora
+const calculator = new Calculadora(prevResultText, resultText)
